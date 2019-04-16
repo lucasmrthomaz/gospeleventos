9d79e5fad6ccd24f523b77456acefee61166fecc
Lucas Thomaz <lucasmrthomaz@gmail.com>
2019-04-16 16:28:49 -0300

    list pdo test

--- tests/pdo_tests/list_pdo_test.php
+++ tests/pdo_tests/list_pdo_test.php
@@ -5,8 +5,9 @@ $stmt = $dsn->prepare("SELECT * FROM test");
 $stmt->execute(); //Executa a query
 
 // Retorna um statement com array associativo
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    print_r($result['nome']);    echo '<br>';
-    print_r($result['email']);   echo '<br>'; 
-    print_r($result['empresa']); echo '<br>';
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+print_r($result['nome']);    echo '<br>';
+print_r($result['email']);   echo '<br>'; 
+print_r($result['empresa']); echo '<br>';    
+
 ?>
