04cee61dbc0e2c99af0346ae939fe94b4d63439e
Lucas Thomaz <lucasmrthomaz@gmail.com>
2019-04-27 14:51:58 -0300

    Checagem de login manual

--- admin/check_login.php
+++ admin/check_login.php
@@ -1,3 +1,15 @@
 <?php
 
+$email = $_POST['email_i'];
+$senha = $_POST['senha_i'];
+    
+echo 'Email: '.$email;
+echo 'Senha: '.$senha;
+
+if($email == 'teste@teste.com' & $senha == '1234'){
+    echo 'Login permitido com sucesso';
+    echo '';
+}else{
+    echo 'Login nao permitido.';
+}
 ?>
--- admin/index.php
+++ admin/index.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Admin Login</title>
+<title>Login</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
@@ -34,13 +34,12 @@
 </head>
 <body>
 <div class="login-form">
-    <form action="check_login.php" method="post">
+    <form action="check_login.php" method="POST">
         <h2 class="text-center">Login</h2>       
         <div class="form-group">
-            <input type="text" class="form-control" placeholder="Email" required="required">
-        </div>
-        <div class="form-group">
-            <input type="password" class="form-control" placeholder="Senha" required="required">
+            <input name="email_i" type="text" class="form-control" placeholder="Email" required="required">
+            </br>
+            <input name="senha_i" type="password" class="form-control" placeholder="Senha" required="required">
         </div>
         <div class="form-group">
             <button type="submit" class="btn btn-primary btn-block">Entrar</button>
@@ -53,4 +52,4 @@
     <p class="text-center"><a href="#">Criar uma conta</a></p>
 </div>
 </body>
-</html>                                		                            
+</html> 
