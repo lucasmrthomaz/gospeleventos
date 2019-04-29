2568eeb4e2faad1f5168c0b18e59146c4432acf4
Lucas Thomaz <lucasmrthomaz@gmail.com>
2019-04-29 10:59:27 -0300

    checagem de login

--- admin/check_login.php
+++ admin/check_login.php
@@ -1,4 +1,6 @@
 <?php
+// Importa configurações e banco de dados
+include_once('../config/database.php');
 
 $email = $_POST['email_i'];
 $senha = $_POST['senha_i'];
@@ -6,10 +8,15 @@ $senha = $_POST['senha_i'];
 echo 'Email: '.$email;
 echo 'Senha: '.$senha;
 
+ 
 if($email == 'teste@teste.com' & $senha == '1234'){
     echo 'Login permitido com sucesso';
-    echo '';
 }else{
     echo 'Login nao permitido.';
 }
+
+}
+
+
+
 ?>
