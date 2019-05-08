474e9e6b78a4ef8ae4ddcffe32f315e2d4abc0cb
Lucas Thomaz <lucasmrthomaz@gmail.com>
2019-05-08 15:09:30 -0300

    separação dos componentes da index

--- index.php
+++ index.php
@@ -25,51 +25,17 @@
 
   <!-- CSS do Slider -->
 <link href="css/custom.css" rel="stylesheet">
+<!-- Importa o CSS do Slider principal -->
+<link href="css/index_footer.css" rel="stylesheet">
 
+<style>
+
+</style>
 </head>
 <body>
 <?php include_once('navbar.php'); ?>
+<?php include_once('index_slider.php');?>
 
-<!-- Começo do Slider -->
-<div id="carouselFull" class="carousel slide" data-ride="carousel">
-       <ol class="carousel-indicators">
-           <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
-           <li data-target="#carouselIndicators" data-slide-to="1"></li>
-           <li data-target="#carouselIndicators" data-slide-to="2"></li>
-       </ol>
-       <div class="carousel-inner">
-           <div class="carousel-item active">
-               <img class="d-block" src="img/s1.jpg" alt="First slide">
-               <div class="carousel-caption d-md-block">
-                   <h3>Seja Bem-vindo ao Gospel Eventos</h3>
-                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer felis neque, suscipit eget dolor quis, accumsan imperdiet elit. Praesent quis mauris</p>
-               </div>
-           </div>
-           <div class="carousel-item">
-               <img class="d-block" src="img/s2.jpg" alt="Second slide">
-               <div class="carousel-caption d-md-block">
-                   <h3>Aqui você encontra eventos e muito mais!</h3>
-                   <p>Nullam at elementum felis, at sodales diam. In a lectus nisl. Maecenas sodales commodo sollicitudin. Sed nisi nisl, laoreet eu erat vel, porttitor scelerisque</p>
-               </div>
-           </div>
-           <div class="carousel-item">
-               <img class="d-block" src="img/s3.jpg" alt="Third slide">
-               <div class="carousel-caption d-md-block">
-                   <h3>Compra de ingressos e reserva online</h3>
-                   <p>Nam scelerisque molestie cursus. Ut scelerisque turpis iaculis erat feugiat, eget tristique risus luctus. Quisque non est dignissim, rhoncus nisi a, fermentum</p>
-               </div>
-           </div>
-       </div>
-       <a class="carousel-control-prev" href="#carouselFull" role="button" data-slide="prev">
-           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-           <span class="sr-only">Previous</span>
-       </a>
-       <a class="carousel-control-next" href="#carouselFull" role="button" data-slide="next">
-           <span class="carousel-control-next-icon" aria-hidden="true"></span>
-           <span class="sr-only">Next</span>
-       </a>
-   </div>
-   <!-- Fim do Slider--> 
 
   <div class="jumbotron">
     <h1 class="display-4">Olá, seja bem-vindo!</h1>
@@ -86,41 +52,7 @@
   
   </div>
 
-<footer>
-<div style="width:100%; height:80px; background-color:black; color:white;"> 
-
-<div class="row">
-  <div class="col-md-4">
-  bla bla bla bla bla bla
-  bla bla bla bla bla bla
-  bla bla bla bla bla bla
-  bla bla bla bla bla bla
- </br>
- evento5 evento6 evento7 evento8
-  </div>
-
-  <div class="col-md-4">
-  bla bla bla bla bla bla
-  bla bla bla bla bla bla
-  bla bla bla bla bla bla
-  bla bla bla bla bla bla
- </br>
- evento5 evento6 evento7 evento8
-  </div>
-
-  <div class="col-md-4">
-  bla bla bla bla bla bla
-  bla bla bla bla bla bla
-  bla bla bla bla bla bla
-  bla bla bla bla bla bla
- </br>
- evento5 evento6 evento7 evento8
-  </div>
- 
- </div>
- </footer>
-
-
+<?php include_once('index_footer.php'); ?>
 </body>
 
 </html>
---
+++ index_footer.php
@@ -0,0 +1,61 @@
+<footer>
+<!-- Footer -->
+<section id="footer">
+		<div class="container">
+			<div class="row text-center text-xs-center text-sm-left text-md-left">
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<h5>Categorias</h5>
+					<ul class="list-unstyled quick-links">
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Festas, Shows e Festivais</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Palestras e Conferencias </a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Alternativos </a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Retiros e viagens</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Música Excurções</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Esportivos</a></li>
+            <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Gastronomicos</a></li>
+          </ul>
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<h5>Bloco de links</h5>
+					<ul class="list-unstyled quick-links">
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i></a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i></a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i></a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i></a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i></a></li>
+					</ul>
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<h5>Eventos neste fim de semana</h5>
+					<ul class="list-unstyled quick-links">
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i></a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i></a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i></a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i></a></li>
+						<li><a href="https://wwwe.sunlimetech.com" title="Design and developed by"><i class="fa fa-angle-double-right"></i></a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+					<ul class="list-unstyled list-inline social text-center">
+						<li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-facebook"></i></a></li>
+						<li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-twitter"></i></a></li>
+						<li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-instagram"></i></a></li>
+						<li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-google-plus"></i></a></li>
+						<li class="list-inline-item"><a href="javascript:void();" target="_blank"><i class="fa fa-envelope"></i></a></li>
+					</ul>
+				</div>
+				</hr>
+			</div>	
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+					Todos direitos reservados &copy 2019.
+				</div>
+				</hr>
+			</div>	
+		</div>
+	</section>
+	<!-- ./Footer -->
+
+ </footer>
